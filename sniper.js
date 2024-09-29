fbf3fceb58b0a6eeb9b9d8a203890030b55f0d74
Mentalite <edisjokear@gmail.com>
2024-09-29 13:31:01 +0200

    Update sniper.js

--- sniper.js
+++ sniper.js
@@ -1,12 +1,9 @@
 const WebSocket = require('ws');
 const config = require('./config.json');
 const fetch = require('node-fetch');
-
 let socket = null;
 let heartbeatInterval = null;
-
 const guilds = {};
-
 const sendWebhookLog = async (message) => {
     try {
         await fetch(config.WEBHOOK_URL, {
@@ -23,10 +20,8 @@ const sendWebhookLog = async (message) => {
         console.error('Error sending webhook log:', error.message);
     }
 };
-
 const claimVanityURL = async (vanityURL) => {
     const url = `https://canary.discord.com/api/v9/guilds/${config.GUILD_ID}/vanity-url`;
-
     try {
         const response = await fetch(url, {
             method: 'PATCH',
@@ -36,7 +31,6 @@ const claimVanityURL = async (vanityURL) => {
             },
             body: JSON.stringify({ code: vanityURL })
         });
-
         if (response.ok) {
             console.log(`Successfully claimed vanity URL: ${vanityURL}`);
             await sendWebhookLog(`Successfully claimed vanity URL: ${vanityURL}`);
@@ -47,45 +41,36 @@ const claimVanityURL = async (vanityURL) => {
         console.error('Error claiming vanity URL:', error.message);
     }
 };
-
 const handleGuildUpdate = (guildData) => {
     const oldVanity = guilds[guildData.id] ? guilds[guildData.id].vanity_url_code : null;
     const newVanity = guildData.vanity_url_code;
-
     if (!newVanity && oldVanity) {
         console.log(`Vanity URL ${oldVanity} is now free! Attempting to claim...`);
         claimVanityURL(oldVanity);
     }
-
     guilds[guildData.id] = { vanity_url_code: newVanity };
 };
-
 const onMessage = async (message) => {
     const data = JSON.parse(message);
-
     switch (data.op) {
         case 10: 
             startHeartbeat(data.d.heartbeat_interval);
             identifyConnection();
             break;
-
         case 0: 
             if (data.t === 'GUILD_UPDATE') {
                 handleGuildUpdate(data.d);
             }
             break;
-
         default:
             console.log(`Unknown WebSocket message received: ${data.op}`);
     }
 };
-
 const startHeartbeat = (interval) => {
     heartbeatInterval = setInterval(() => {
         socket.send(JSON.stringify({ op: 1, d: null }));
     }, interval);
 };
-
 const identifyConnection = () => {
     socket.send(JSON.stringify({
         op: 2,
@@ -100,26 +85,20 @@ const identifyConnection = () => {
         }
     }));
 };
-
 const initWebSocketConnection = () => {
     socket = new WebSocket('wss://gateway.discord.gg/?v=9&encoding=json');
-
     socket.on('open', () => {
         console.log('WebSocket connection established.');
     });
-
     socket.on('message', onMessage);
-
     socket.on('close', () => {
         console.log('WebSocket connection closed. Reconnecting...');
         clearInterval(heartbeatInterval);
         setTimeout(initWebSocketConnection, 1000);
     });
-
     socket.on('error', (error) => {
         console.error('WebSocket encountered an error:', error.message);
         clearInterval(heartbeatInterval);
     });
 };
-
 initWebSocketConnection();
